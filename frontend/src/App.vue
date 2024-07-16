b58aaf06b674b1cb0b6200f1a9b96afc93cfa628
SHAWN09L <156237935+SHAWN09L@users.noreply.github.com>
2024-07-16 14:25:31 -0400

    base app.vue et main.js

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <router-view></router-view>
+</template>
+
+<script>
+
+</script>
+
+
+<style>
+* {
+    box-sizing: border-box;
+}
+
+html {
+    font-family: sans-serif;
+}
+</style>
