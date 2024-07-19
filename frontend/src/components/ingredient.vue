7d22b96267a1bf47fa01f39025b4bddb63486783
Xavier00014 <131310111+Xavier00014@users.noreply.github.com>
2024-07-19 14:09:00 -0400

    affichage d'une recette

---
+++ frontend/src/components/ingredient.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        {{ nom }} : {{ quantier }}
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        nom: String,
+        quantier: String,
+    }
+}
+</script>
--- frontend/src/pages/RecetteDetaillee.vue
+++ frontend/src/pages/RecetteDetaillee.vue
@@ -1,16 +1,56 @@
 <template>
-    <h2>{{recetteKey}}</h2>
+    <h2>{{nom}}</h2>
     <img src="" alt="image recette">
-    <p>Temps de préparation</p>
-    <p>Temps de cuisson</p>
-    <p>Nombre de portion</p>
-    <p>Description</p>
-    <ingrédient></ingrédient>
-    <etape></etape>
+    <p>Temps de préparation: {{ tempsDePrep }} minute</p>
+    <p>Temps de cuisson: {{ tempsDeCuit }}</p>
+    <p>Nombre de portion: {{ portion }}</p>
+    <p>{{ description }}</p>
+    <Ingredient v-for="ingredient in ingredients" :nom=ingredient.nom :quantier=ingredient.quantier></Ingredient>
+    <etape v-for="(etape, id) in etapes" :numero="id+1" :description="etape.description"></etape>
 </template>
 
 <script>
+import Ingredient from '../components/ingredient.vue';
+import Etape from '../components/etape.vue';
+
 export default {
+    components: {
+        Ingredient,
+        Etape
+    },
+    data: function () {
+        return {
+            nom: 'Spagettie',
+            tempsDePrep: 1,
+            tempsDeCuit: 10,
+            portion: 4,
+            description: "C'est du spag",
+            ingredients: [
+                {
+                    nom: "pate",
+                    quantier: "1 boite"
+                },
+                {
+                    nom: "sause",
+                    quantier: "1 pot"
+                }
+            ],
+            etapes: [
+                {
+                    description: "faire bouillir de l'eau"
+                },
+                {
+                    description: "mettre les pate dans l'eau"
+                },
+                {
+                    description: "faire chaufer la sauce"
+                },
+                {
+                    description: "mellenger"
+                }
+            ]
+        }
+    },
     props: {
         recetteKey: String
     },
