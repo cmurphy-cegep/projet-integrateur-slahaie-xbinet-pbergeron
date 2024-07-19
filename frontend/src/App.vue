2827461701503b8cb7aba5aec845d274053711a1
SHAWN09L <156237935+SHAWN09L@users.noreply.github.com>
2024-07-19 13:34:38 -0400

    test

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,15 @@
 <template>
+    <FormulaireLogin/>
     <router-view></router-view>
 </template>
 
 <script>
-
+import FormulaireLogin from './pages/FormulaireLogin.vue';
+export default {
+    components: {
+        FormulaireLogin
+    }
+}
 </script>
 
 
---
+++ frontend/src/pages/FormulaireLogin.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="boxed-left">
+        <form>
+            <div>
+                <label for="utilisateur">Identifiant utilisateur:</label>
+                <input id="utilisateur" v-model="utilisateur">
+            </div>
+            <div>
+                <label for="motDePasse">Mot de passe</label>
+                <input type="password" id="motDePasse" v-model="motDePasse">
+            </div>
+            <button>Se connecter</button>
+        </form>
+    </div>
+</template>
+
+<script>
+import session from '../session';
+
+export default {
+    data: function () {
+        return {
+            utilisateur: '',
+            motDePasse: ''
+        };
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<style scoped>
+form * {
+    margin: 0.3rem;
+}
+
+.boxed-left {
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    margin: 1rem auto;
+    border-radius: 10px;
+    padding: 1rem;
+    text-align: left;
+    width: 90%;
+    max-width: 80rem;
+}
+</style>
