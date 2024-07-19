39ef819b84180ae9c1db8c6a02463842905686b0
Xavier00014 <131310111+Xavier00014@users.noreply.github.com>
2024-07-19 13:35:33 -0400

    création de la page recette

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,14 @@
 import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
+import RecetteDetaillee from './pages/RecetteDetaillee.vue';
 
 const app = createApp(App);
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        
+        { path: '/recettes/:recetteKey', component: RecetteDetaillee, props: true },
     ]
 });
 
---
+++ frontend/src/pages/RecetteDetaillee.vue
@@ -0,0 +1,18 @@
+<template>
+    <h2>{{recetteKey}}</h2>
+    <img src="" alt="image recette">
+    <p>Temps de préparation</p>
+    <p>Temps de cuisson</p>
+    <p>Nombre de portion</p>
+    <p>Description</p>
+    <ingrédient></ingrédient>
+    <etape></etape>
+</template>
+
+<script>
+export default {
+    props: {
+        recetteKey: String
+    },
+}
+</script>
