c3355f9e33207d2e5f3379e316a7208ad633d783
SHAWN09L <156237935+SHAWN09L@users.noreply.github.com>
2024-08-04 16:39:14 -0400

    insertdonnees test

---
+++ Base-de-données/insertDonnees.sql
@@ -0,0 +1,6 @@
+Insert INTO Recettes (id_recette, nom_recette, description_courte,
+ description, temps_preparation, temps_cuisson, nb_portions, image)
+ VALUES 
+ ('Spaghetti_Carbonara', 'Spaghetti Carbonara', 'Ce spaghetti à la carbonara est un plat de spaghettis « bacon et œuf » classique et très riche, idéal à servir en compagnie...',
+ 'Ce spaghetti à la carbonara est un plat de spaghettis « bacon et œuf » classique et très riche, idéal à servir en compagnie. Cette recette constitue également une offre de brunch inhabituelle.',
+ 20, 20, 8, 'spaghetti_carbonara.jpg')
