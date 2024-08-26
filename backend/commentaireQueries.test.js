433cb3d75b4e72c9d0bd6383a5db3789529f9ec6
Otoko-Senpai <philippebergeron47@gmail.com>
2024-08-26 10:42:14 -0400

    Update commmentaire avec test bdd

--- backend/commentaireQueries.test.js
+++ backend/commentaireQueries.test.js
@@ -1,28 +1,28 @@
 const commentaireQueries = require('./queries/commentaireQueries.js');
 const pool = require('./queries/DBPool.js');
-jest.mock('./queries/DBPool.js');
+
 
 
 
 describe("Testing Commentary", function () {
-    test("expect user to be the user", async function () {
-        pool.query.mockResolvedValue({
-            rows: [{
-                commentaire: "Premier commentaire",
-                date_publication: "2020-08-20",
-                id_utilisateurs: "admin"
-            }]
-        });
-        let commentaires = await commentaireQueries.getCommentaire('Spaghetti_Carbonara');
-
-        expect(commentaires[0].utilisateur).toBe('admin');
-        
-    });
     test("testing insertion of a commentary",async function () {
+        let id_recette = 'Spaghetti_Carbonara';
+        let id_utilisateur = 'admin';
+        let commentaire = 'deuxième commentaire';
 
+        let results = await pool.query(
+            `SELECT MAX(id_commentaire) FROM commentaires`
+        );
+        let max = results.rows[0].max;
+        let id_commentaire  = await commentaireQueries.postCommentaire(id_recette,id_utilisateur,commentaire);
+
+
+
+
+        expect(id_commentaire).toBe(max + 1);
 
     });
     afterAll(async () => {
-        pool.query.mockRestore();
+        await pool.end();
     });
 });
